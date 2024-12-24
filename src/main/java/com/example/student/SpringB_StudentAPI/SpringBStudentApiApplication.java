4698ed0138c130feb2e0837844bd0e0914d2424e
AK-Malik <akmalik.it@gmail.com>
2024-12-24 14:51:18 -0500

    Entered a print statement under SpringbStudentApiApplication.java class

---
+++ src/main/java/com/example/student/SpringB_StudentAPI/SpringBStudentApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.student.SpringB_StudentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBStudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBStudentApiApplication.class, args);
+	}
+	System.out.println ("Hello World, This is the starting point of my SpringB_StudentAPI Springboot Web Application");
+
+}
---
+++ src/test/java/com/example/student/SpringB_StudentAPI/SpringBStudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student.SpringB_StudentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBStudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
